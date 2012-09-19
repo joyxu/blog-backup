88d027189c9ae414e60969c6829eb006d2805a40
joyxu <xuwei04@gmail.com>
2012-09-19 12:19:41 +0800

    0.1 version

---
+++ js/blog.js
@@ -0,0 +1,100 @@
+var JoyXu = JoyXu || {
+    Blog:{}
+}
+
+JoyXu.Blog = function(){
+    this.indexPage = encodeURI("file:///source/blog/post/index.json");
+    this.col = 4;
+    this.tips = [];
+    this.showTipDiv = "";
+
+}
+
+JoyXu.Blog.prototype.initPerson = function(parentid){
+
+}
+
+JoyXu.Blog.prototype.setTipDetailDiv = function(parentid){
+    this.showTipDiv = parentid;
+    $("#" + this.showTipDiv).click(function(){
+        $("#" + parentid).hide();
+    });
+}
+
+JoyXu.Blog.prototype.initTips = function(parentid){
+    var that = this;
+    $.ajax({
+        url: that.indexPage ,
+        success: function(data){
+            var tips = eval(data);
+            var index = 0;
+            var tip;
+            var html = "";
+            var param = {};
+            for( index = 0 ; index < tips.length ; index++){
+                param.id = index;
+                param.title = tips[index].title;
+                param.content = tips[index].content;
+                param.date = tips[index].date;
+                param.category = tips[index].category;
+                tip = new JoyXu.Tip(param);
+                that.tips.push(tip);
+                if( 0 == index % that.col ){
+                    html += "<ul class='tiprow'>";
+                }
+                html += "<li class='tip'>" + tip.getShortTip() + "</li>";
+                if( this.col - 1 == index % that.col){
+                    html += "</ul>"
+                }
+            }
+            if( this.col - 1 > index % that.col){
+                html += "</ul>"
+            }
+            $("#" + parentid).append(html);
+            $("#" + parentid + "> ul > li").each(function(i){
+                var index = i;
+                $(this).click(function(){
+                    that.setClickOnTip(index);
+                });
+            });
+        }
+    });
+}
+
+JoyXu.Blog.prototype.setClickOnTip = function(index){
+    var tip = this.tips[index];
+    var that = this;
+    $("#" + this.showTipDiv).empty();
+    $.ajax({
+        url: tip.path,
+        success: function(data){
+            var converter = new Showdown.converter();
+            $("#" + that.showTipDiv).append(converter.makeHtml(data));
+            $("#" + that.showTipDiv).css({ 
+                position: 'absolute', 
+                'z-index': 10000, 
+                width: $("body").width(), 
+                height:$("body").height(), 
+            }); 
+            $("#" + that.showTipDiv).show();
+        }
+    });
+}
+
+JoyXu.Tip = function(param){
+    this.id = param.id || "";
+    this.title = param.title || "";
+    this.content = param.content || "";
+    this.date = param.date || "";
+    this.category = param.category || "other";
+    this.path = encodeURI("file:///source/blog/post/" + this.title);
+}
+
+JoyXu.Tip.prototype.getShortTip = function(){
+    var html = "<article><header></header><a><h3>" + this.title + "</h3></a><footer>";
+    for(var i in this.category){
+        html += "<a>" + this.category[i]+ "</a>";
+    }
+    html += "</footer></article>";
+    return html;
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
+var blog;
 $(function(){
-    var converter = new Showdown.converter();
-
-    var LoadDiaryByPath = function(path){
-        var fp = new File(path);
-        var content = "";
-        if(fp.open("r")){
-            content = fp.read(fp.getLength());
-            fp.close();
-        }
-        return content;
-    }
-
-    $("#blogcontent").append(converter.makeHtml(LoadDiaryByPath("../post/20120914-github建个人blog")));
+    blog = new JoyXu.Blog();
+    blog.initTips("tips");
+    blog.setTipDetailDiv("tipdetail");
 });
